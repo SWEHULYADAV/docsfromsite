23342a407ba0e8a9f1bf9decfeed503206742892
SWEHULYADAV <111967695+SWEHULYADAV@users.noreply.github.com>
2022-08-26 15:40:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,9 @@ while True:
             err_count = 0
             url = "https://indiankanoon.org/doc/"+str(pgcount)  #link of the site which we scrapped 
             driver.get(url)
+            print(driver.page_source)
             #variable with xpath 
-            search = WebDriverWait(driver, 50).until(
+            search = WebDriverWait(driver, 120).until(
         EC.presence_of_element_located((By.XPATH, "//input[@value='Get this document in PDF']")))
             print(driver.page_source)
             search.click() #automating code to click and download Docs
