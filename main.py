874916e90b07129c1f54aed809f01b76ea9c080d
SWEHULYADAV <111967695+SWEHULYADAV@users.noreply.github.com>
2022-08-26 15:04:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox") 
-driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), options=chrome_options)driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), options=chrome_options)
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 driver.page_source
 #creating variable
 pgcount = 0
