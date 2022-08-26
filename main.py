060578af796894b9da101d6e866f1c1279167681
SWEHULYADAV <111967695+SWEHULYADAV@users.noreply.github.com>
2022-08-26 13:02:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 
 
@@ -11,7 +13,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox") 
 driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), options=chrome_options)
-
+driver.page_source
 #creating variable
 pgcount = 0
 err_count = 0
@@ -25,10 +27,10 @@ while True:
             err_count = 0
             url = "https://indiankanoon.org/doc/"+str(pgcount)  #link of the site which we scrapped 
             driver.get(url)
-            sleep(50)
+            #variable with xpath 
+            search = WebDriverWait(driver, 50).until(
+        EC.presence_of_element_located((By.XPATH, "//input[@value='Get this document in PDF']")))
             print(driver.page_source)
-            #variable with xpath
-            search = driver.find_element(By.XPATH, "//input[@value='Get this document in PDF']")  
             search.click() #automating code to click and download Docs
             print("DOCUMENT NO: "+ str(pgcount) +" DOWNLOADED")
         except:      #exceptional condition
